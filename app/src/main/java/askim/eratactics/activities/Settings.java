5e3bf42d985639ea25b39a850e1d6c0bc774d63d
Addy Kim <soyaddyk@gmail.com>
2016-03-24 22:06:39 -0500

    Created initial settings activity

--- app/src/main/java/askim/eratactics/activities/Settings.java
+++ app/src/main/java/askim/eratactics/activities/Settings.java
@@ -2,14 +2,21 @@ package askim.eratactics.activities;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
 import askim.eratactics.R;
 
-public class Settings extends Activity {
+public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.hide();
     }
 
 }
