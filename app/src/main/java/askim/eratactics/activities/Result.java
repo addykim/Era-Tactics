a9d221b95f4b281cb13783e91b6e6ee8866588f3
Addy Kim <soyaddyk@gmail.com>
2016-04-04 14:59:51 -0500

    Grab intent pt 1

--- app/src/main/java/askim/eratactics/activities/Result.java
+++ app/src/main/java/askim/eratactics/activities/Result.java
@@ -17,16 +17,18 @@ public class Result extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
+//        Intent myIntent = getIntent(); // gets the previously created intent
+//        String firstKeyName = myIntent.getStringExtra("firstKeyName");
         text = (TextView) findViewById(R.id.result_text);
         backButton = (TextView) findViewById(R.id.back_to_splash);
         //TODO grab win status from last activity
+        boolean win = true;
+        if (win) {
+            text.setText(R.string.postGameCleared);
+        } else {
+            text.setText(R.string.postGameLose);
+        }
 
-        // if win
-        text.setText(R.string.postGameCleared);
-        // else
-        text.setText(R.string.postGameLose);
-
-
+        //TODO back button go back to splash screen
     }
-
 }
