3420b9cb6bea4f16019d7c00fd24b65aedb90fba
Addy Kim <soyaddyk@gmail.com>
2016-04-11 18:24:46 -0500

    Fixed syntax errors in settings

--- app/src/main/java/askim/eratactics/activities/Settings.java
+++ app/src/main/java/askim/eratactics/activities/Settings.java
@@ -1,19 +1,21 @@
 package askim.eratactics.activities;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 import askim.eratactics.R;
 
 public class Settings extends AppCompatActivity {
 
-//    TODO these may not be buttons
-//    private Button
-    private Button vibration;
-    private Button rHandMode;
+    SharedPreferences settings;
+    SharedPreferences.Editor editor;
+    private static final String PREFS_NAME = "et_prefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +23,17 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         
         // Code from http://stackoverflow.com/questions/5734721/android-shared-preferences
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        SharedPreferences.Editor editor = settings.edit();
+        settings = getSharedPreferences(PREFS_NAME, 0);
+        editor = settings.edit();
         
         // Toggle button http://developer.android.com/guide/topics/ui/controls/togglebutton.html
         ToggleButton vibrationToggle = (ToggleButton) findViewById(R.id.vibrationToggle);
         vibrationToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     // The toggle is enabled
-                    editor.putBoolean("vibration"), true);
+                    editor.putBoolean("vibration", true);
                 } else {
                     // The toggle is disabled
                     editor.putBoolean("vibration", false);
@@ -41,11 +44,13 @@ public class Settings extends AppCompatActivity {
         editor.commit();
 
         //code for getting shared preferences
-        SharedPreferences settings = getSharedPreferences(SignIn.PREFS_NAME,
-                Activity.MODE_PRIVATE);
+        settings = getSharedPreferences(PREFS_NAME, Activity.MODE_PRIVATE);
+        vibrationToggle = (ToggleButton) findViewById(R.id.vibrationToggle);
+        settings.getBoolean("vibration", false);
         // username = (TextView) findViewById(R.id.username);
         // String uname = settings.getString("user_name", null);
         // username.setText(uname);
+
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null)
