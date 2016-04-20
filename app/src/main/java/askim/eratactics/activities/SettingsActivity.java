0215ac80d59ceb108c7bcd8c22e5b8b4c1e767df
Addy Kim <soyaddyk@gmail.com>
2016-04-19 22:38:47 -0500

    Settings now sets and receives properly

--- app/src/main/java/askim/eratactics/activities/SettingsActivity.java
+++ app/src/main/java/askim/eratactics/activities/SettingsActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import askim.eratactics.R;
@@ -16,85 +18,96 @@ public class SettingsActivity extends AppCompatActivity {
     SharedPreferences settings;
     SharedPreferences.Editor editor;
     public static final String PREFS_NAME = "et_prefs";
+    private static final String TAG = "Settings";
+
+    // Toggles
+    private ToggleButton musicToggle;
+    private ToggleButton vibrationToggle;
+    private ToggleButton sfxToggle;
+    private ToggleButton leftToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.hide();
+
         // Code from http://stackoverflow.com/questions/5734721/android-shared-preferences
-        settings = getSharedPreferences(PREFS_NAME, 0);
+        settings = getSharedPreferences(PREFS_NAME, Activity.MODE_PRIVATE);
+        musicToggle = (ToggleButton) findViewById(R.id.musicToggle);
+        sfxToggle = (ToggleButton) findViewById(R.id.sfxToggle);
+        vibrationToggle = (ToggleButton) findViewById(R.id.vibrationToggle);
+        leftToggle = (ToggleButton) findViewById(R.id.leftHandedToggle);
+
+        // Set checked
+        musicToggle.setChecked(settings.getBoolean("music", true));
+        sfxToggle.setChecked(settings.getBoolean("sfx", true));
+        vibrationToggle.setChecked(settings.getBoolean("vibration", false));
+        leftToggle.setChecked(settings.getBoolean("leftMode", false));
+
         editor = settings.edit();
 
         // Toggle button http://developer.android.com/guide/topics/ui/controls/togglebutton.html
-        ToggleButton musicToggle = (ToggleButton) findViewById(R.id.musicToggle);
         musicToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d(TAG, "Music toggle: " + isChecked);
                 if (isChecked) {
                     editor.putBoolean("music", true);
                 } else {
                     editor.putBoolean("music", false);
                 }
+                Log.d(TAG, "Commiting");
+                editor.commit();
             }
         });
 
-        ToggleButton sfxToggle = (ToggleButton) findViewById(R.id.vibrationToggle);
         sfxToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d(TAG, "SFX toggle: " + isChecked);
                 if (isChecked) {
                     editor.putBoolean("sfx", true);
                 } else {
                     editor.putBoolean("sfx", false);
                 }
+                Log.d(TAG, "Commiting");
+                editor.commit();
             }
         });
 
-        ToggleButton vibrationToggle = (ToggleButton) findViewById(R.id.vibrationToggle);
         vibrationToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d(TAG, "Vibration toggle: " + isChecked);
                 if (isChecked) {
+                    Toast.makeText(getApplicationContext(), "Sorry vibration mode isn't available yet :(", Toast.LENGTH_LONG).show();
                     editor.putBoolean("vibration", true);
                 } else {
                     editor.putBoolean("vibration", false);
                 }
+                Log.d(TAG, "Commiting");
+                editor.commit();
             }
         });
 
-        ToggleButton leftToggle = (ToggleButton) findViewById(R.id.vibrationToggle);
         leftToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d(TAG, "left toggle: " + isChecked);
                 if (isChecked) {
+                    Toast.makeText(getApplicationContext(), "Sorry left handed mode isn't available yet :(", Toast.LENGTH_LONG).show();
                     editor.putBoolean("leftMode", true);
                 } else {
                     editor.putBoolean("leftMode", false);
                 }
+                Log.d(TAG, "Commiting");
+                editor.commit();
             }
         });
-        editor.apply();
-        // Change to commit if we will be changing music settings from other portions of the game
-//        editor.commit();
-
-        //code for getting shared preferences
-        settings = getSharedPreferences(PREFS_NAME, Activity.MODE_PRIVATE);
-
-        Boolean toggle = settings.getBoolean("music", true);
-        musicToggle.setChecked(toggle);
-
-        toggle = settings.getBoolean("sfx", true);
-        sfxToggle.setChecked(toggle);
-
-        toggle = settings.getBoolean("vibration", false);
-        vibrationToggle.setChecked(toggle);
-
-        toggle = settings.getBoolean("leftMode", false);
-        leftToggle.setChecked(toggle);
-
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null)
-            actionBar.hide();
     }
 }
+
+
