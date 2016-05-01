413618cb9fdb44bb1b1c9bff715505acc9206be8
Addy Kim <soyaddyk@gmail.com>
2016-05-01 02:00:53 -0500

    Converted level generator to db style

--- app/src/main/java/askim/eratactics/gamelogic/LevelGenerator.java
+++ app/src/main/java/askim/eratactics/gamelogic/LevelGenerator.java
@@ -1,44 +1,68 @@
 package askim.eratactics.gamelogic;
 
+import android.content.Context;
+
+import com.orm.SugarRecord;
+
 /**
  * Created by nunuloop on 4/19/16.
  */
-public class LevelGenerator {
+public class LevelGenerator extends SugarRecord {
 
-    public static Piece[][] generate(int lvl) {
-        Piece[][] enemyPieces = new Piece[3][3];
+    private String name;
+    private Piece[][] enemyPieces;
+    private boolean cleared;
 
-        switch (lvl) {
-            case 1:
-                // 3 enemies
-                enemyPieces[0][0] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
-                enemyPieces[0][1] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
-                enemyPieces[0][2] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
-                break;
-            case 2:
-                // 4 enemies
-                enemyPieces[0][0] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
-                enemyPieces[0][2] = new Piece(EnumFile.ClassEnum.BADTEETH);
-                enemyPieces[1][1] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
-                enemyPieces[2][2] = new Piece(EnumFile.ClassEnum.BADTEETH);
-                break;
-            case 3:
-                // 4 enemies
-                enemyPieces[0][0] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
-                enemyPieces[0][1] = new Piece(EnumFile.ClassEnum.BADTEETH);
-                enemyPieces[0][2] = new Piece(EnumFile.ClassEnum.BADTEETH);
-                enemyPieces[1][1] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
-                enemyPieces[2][2] = new Piece(EnumFile.ClassEnum.BADTEETH);
-                break;
-            case 4:
-                // 4 enemies
-                enemyPieces[0][0] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
-                enemyPieces[0][2] = new Piece(EnumFile.ClassEnum.BADTEETH);
-                enemyPieces[1][1] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
-                enemyPieces[2][2] = new Piece(EnumFile.ClassEnum.BADTEETH);
-                enemyPieces[2][1] = new Piece(EnumFile.ClassEnum.BADTEETH);
-                break;
-        }
-        return enemyPieces;
+    public LevelGenerator() {
+
     }
+
+    public LevelGenerator(Context c) {
+
+        // 3 enemies
+        setAllNull();
+        name = "Level 1";
+        enemyPieces[0][0] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
+        enemyPieces[0][1] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
+        enemyPieces[0][2] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
+        this.save();
+
+        // 4 enemies
+        setAllNull();
+        name = "Level 2";
+        enemyPieces[0][0] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
+        enemyPieces[0][2] = new Piece(EnumFile.ClassEnum.BADTEETH);
+        enemyPieces[1][1] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
+        enemyPieces[2][2] = new Piece(EnumFile.ClassEnum.BADTEETH);
+        this.save();
+
+        // 4 enemies
+        setAllNull();
+        name = "Level 3";
+        enemyPieces[0][0] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
+        enemyPieces[0][1] = new Piece(EnumFile.ClassEnum.BADTEETH);
+        enemyPieces[0][2] = new Piece(EnumFile.ClassEnum.BADTEETH);
+        enemyPieces[1][1] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
+        enemyPieces[2][2] = new Piece(EnumFile.ClassEnum.BADTEETH);
+        this.save();
+
+        // 4 enemies
+        setAllNull();
+        name = "Level 4";
+        enemyPieces[0][0] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
+        enemyPieces[0][2] = new Piece(EnumFile.ClassEnum.BADTEETH);
+        enemyPieces[1][1] = new Piece(EnumFile.ClassEnum.HORNEDFROG);
+        enemyPieces[2][2] = new Piece(EnumFile.ClassEnum.BADTEETH);
+        enemyPieces[2][1] = new Piece(EnumFile.ClassEnum.BADTEETH);
+        this.save();
+    }
+
+    public void setAllNull() {
+        name = null;
+        enemyPieces = new Piece[3][3];
+    }
+
+    public String getName() { return name; }
+
+    public boolean getCleared() { return cleared; }
 }
